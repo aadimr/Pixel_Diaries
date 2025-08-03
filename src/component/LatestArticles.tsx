5a1a5dfd1ebaf9c130fe02f5cf683c87214f9083
Aditya Shaw <adityashaw9066@gmail.com>
2025-08-03 16:34:21 +0530

    working

--- src/component/LatestArticles.tsx
+++ src/component/LatestArticles.tsx
@@ -1,6 +1,44 @@
+import React from 'react'
+import LatestArticlesCard from './LatestArticlesCard';
+
+interface Article {
+  heading: string;
+  image: string;
+  shortCont: string;
+}
+
+const data: Article[] = [
+  {
+    heading: "abc",
+    image: "/image1.jpg",
+    shortCont: "dsgvmndergvnoiuterghervnfdsveriogfmjerfjwasdxmkascpsmdvfregertgvfdvfdgtreghtregb",
+  },
+  {
+    heading: "xyz",
+    image: "/image2.jpg",
+    shortCont: "asdlmoklfjerrgfdjcsnxdjqdneuifjhv dcnvcerkgfjifdvhjdscndkvffcvd vfdvrf",
+  },
+  {
+    heading: "pqr",
+    image: "/image3.avif",
+    shortCont: "alkjopswdfjerkoivnmddsdwlsefnjkcvbsdkcblwefhiusdjkcnbsdjdnbqikjdsweigfndvxcjhsdheiuf",
+  }
+]
+
 function LatestArticles() {
   return (
-    <div>LatestArticles</div>
+    <div className="grid grid-cols-3 gap-10 px-[2rem] pt-[5rem]">
+      {
+        data.map(({ heading, image, shortCont }) => {
+          return <LatestArticlesCard
+            key={heading}
+            heading={heading}
+            image={image}
+            shortCont={shortCont}
+          />
+        })
+      }
+    </div>
   )
 }
 
--- src/component/LatestArticlesCard.tsx
+++ src/component/LatestArticlesCard.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import Image from 'next/image';
+import React, { JSX } from 'react'
 
-function LatestArticlesCard() {
+interface data {
+    heading: string,
+    image: string,
+    shortCont: string
+}
+
+function LatestArticlesCard({ heading, image, shortCont }: data): JSX.Element {
     return (
-        <div>
-            <section>
-
-            </section>
-            <p>Title of the blog</p>
-            <p>date</p>
-            <p>content</p>
-            <p>read more</p>
+        <div className='border-1 rounded-[1rem] p-7'>
+            <div className="relative w-full h-50 mb-2">
+                <Image
+                    src={image}
+                    alt={heading}
+                    fill
+                    sizes="200px"
+                    style={{ objectFit: 'cover' }}
+                    className="rounded-[1rem]"
+                />
+            </div>
+            <h3 className="text-lg font-semibold mb-2">{heading}</h3>
+            <p className="text-sm break-words">{shortCont}</p>
+            <p>Read more...</p>
         </div>
     )
 }
